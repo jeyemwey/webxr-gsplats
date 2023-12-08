bc5e9831ac587097482aaa5b68a01cc64b9c410f
Jannik Volkland <volkland@sipgate.de>
2023-12-08 12:59:53 +0100

    🚧 WIP, add object to test movement and reticle for hit tester

--- src/activateXR.ts
+++ src/activateXR.ts
@@ -2,7 +2,6 @@
 import type {Navigator} from 'webxr';
 
 import * as THREE from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
 export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) => async () => {
@@ -11,7 +10,7 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext
         return;
     }
 
-    const session = await navigator.xr.requestSession("immersive-ar", {requiredFeatures: []});
+    const session = await navigator.xr.requestSession("immersive-ar", {requiredFeatures: ["hit-test"]});
     if (!session) {
         return console.error("Unable to create XR session.");
     }
@@ -19,8 +18,19 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext
 
     const referenceSpace = await session.requestReferenceSpace("local");
 
+    if (!session.requestHitTestSource) {
+        return console.error("Unable to create XR session because the HitTestSource is not defined.");
+    }
+    const hitTestSource = await session.requestHitTestSource({space: await session.requestReferenceSpace("viewer")});
+    if (!hitTestSource) {
+        return console.error("Unable to get hitTestSource.");
+    }
+
     // Create a Scene
     const scene = new THREE.Scene();
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+    directionalLight.position.set(10, 15, 10);
+    scene.add(directionalLight);
 
     // Render Session Base Layer with ThreeJS
     const renderer = new THREE.WebGLRenderer({
@@ -35,7 +45,24 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext
     const camera = new THREE.PerspectiveCamera();
     camera.matrixAutoUpdate = false;
 
+    // Load flower and add to scene, so we can check if scene magic works
+    const loader = new GLTFLoader();
+    loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf",
+        (result) => scene.add(result.scene),
+        undefined,
+        (error) => console.log(`Unable to load Sunflower. ${error}`));
 
+    let reticle: THREE.Group;
+    loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf",
+        (result) => {
+            reticle = result.scene;
+            reticle.visible = false;
+            scene.add(reticle);
+        },
+        undefined,
+        (error) => console.log(`Unable to load Reticle. ${error}`));
+
+    let i = 0;
     const onXrFrame = (_time: DOMHighResTimeStamp, frame: XRFrame) => {
         // Queue up the next frame
         session.requestAnimationFrame(onXrFrame);
@@ -46,7 +73,27 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext
             // in mobile AR, there is only one view
             const view = pose.views[0];
 
-            fetch("/wurst/" + JSON.stringify(view));
+            // Update viewport
+            const viewport = session.renderState.baseLayer!.getViewport(view);
+            if (viewport) {
+                renderer.setSize(viewport.width, viewport.height);
+            } else {
+                console.error("Unable to get viewport.");
+            }
+
+            // Log out the view every hundreds frame
+            if (i++ % 100 === 0) {
+                console.log(view);
+            }
+
+            // Work with Hit Test Results
+            const hitTestResults = frame.getHitTestResults(hitTestSource);
+            if (hitTestResults.length && reticle) {
+                const hitpose = hitTestResults[0].getPose(referenceSpace)!;
+                reticle.visible = true;
+                reticle.position.set(hitpose.transform.position.x, hitpose.transform.position.y, hitpose.transform.position.z);
+                reticle.updateMatrixWorld(true);
+            }
 
             // Update the Camera
             camera.matrix.fromArray(view.transform.matrix);
@@ -54,9 +101,9 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext
             camera.updateMatrixWorld(true);
 
             renderer.render(scene, camera);
+        } else {
+            console.debug("Unable to find pose, dropping frame");
         }
-
-
     };
     session.requestAnimationFrame(onXrFrame);
 };
