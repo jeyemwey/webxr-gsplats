61a0ad92095ab02a67dc88cef17f2e1e0884e248
Jannik Volkland <volkland@sipgate.de>
2023-12-08 12:05:22 +0100

    🚧 WIP, add three.js

--- src/activateXR.ts
+++ src/activateXR.ts
@@ -1,6 +1,10 @@
 // @ts-ignore
 import type {Navigator} from 'webxr';
 
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+
 export const activateXR = (canvas: HTMLCanvasElement, gl: WebGLRenderingContext) => async () => {
     if (!navigator.xr) {
         console.error("The browser does not have an XR component.");
@@ -11,14 +15,31 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGLRenderingContext)
     if (!session) {
         return console.error("Unable to create XR session.");
     }
-    session.updateRenderState({baseLayer: new XRWebGLLayer(session, gl)});
+    await session.updateRenderState({baseLayer: new XRWebGLLayer(session, gl)});
 
-    const referenceSpace = await session.requestReferenceSpace("local-floor");
+    const referenceSpace = await session.requestReferenceSpace("local");
+
+    // Create a Scene
+    const scene = new THREE.Scene();
+
+    // Render Session Base Layer with ThreeJS
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        preserveDrawingBuffer: true,
+        canvas: canvas,
+        context: gl
+    });
+    renderer.autoClear = false;
+
+    // Set up a perspective Camera
+    const camera = new THREE.PerspectiveCamera();
+    camera.matrixAutoUpdate = false;
+
 
     const onXrFrame = (_time: DOMHighResTimeStamp, frame: XRFrame) => {
         // Queue up the next frame
         session.requestAnimationFrame(onXrFrame);
-        gl.bindBuffer(gl.FRAMEBUFFER, session.renderState.baseLayer!.framebuffer);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer!.framebuffer);
 
         const pose = frame.getViewerPose(referenceSpace);
         if (pose) {
@@ -26,6 +47,13 @@ export const activateXR = (canvas: HTMLCanvasElement, gl: WebGLRenderingContext)
             const view = pose.views[0];
 
             fetch("/wurst/" + JSON.stringify(view));
+
+            // Update the Camera
+            camera.matrix.fromArray(view.transform.matrix);
+            camera.projectionMatrix.fromArray(view.projectionMatrix);
+            camera.updateMatrixWorld(true);
+
+            renderer.render(scene, camera);
         }
 
 
