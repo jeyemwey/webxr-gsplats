7279a5fde97705b17417fee95d64570138b78f69
Jannik <jannik@outlook.com>
2024-02-21 22:39:18 +0100

    fix: stay in correct scene when toggling debug mode using :robot:

--- src/debugMode.ts
+++ src/debugMode.ts
@@ -1,6 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 export const isInDebug = urlParams.get('debug') === "true";
+const slug = urlParams.get('slug');
 
 document.getElementById("toggle-debug-link")!
-    .setAttribute("href", `?debug=${(!isInDebug).toString()}`);
+    .setAttribute("href", `?debug=${(!isInDebug).toString()}&slug=${slug}`);
