1bb1b1e947dd9e33198b19fbe6672148ce68d7e6
Jannik <jannik@outlook.com>
2024-02-22 17:31:17 +0100

    fix(static build): restrict to only use ES build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 "./index.html",
                 "./scene-show.html",
                 "./convert-ply.html"
-            ]
+            ],
+            formats: ["es"]
         }
     }
 })
