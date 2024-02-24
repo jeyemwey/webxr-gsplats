64a5721513fccde07be01a4adba89c6950860fb6
Jannik <jannik@outlook.com>
2024-02-24 12:23:09 +0100

    feat: show camera coords in three system in debug mode

--- src/util/stateDistributors/CameraOrientationStateDistributor/positionDisplay.ts
+++ src/util/stateDistributors/CameraOrientationStateDistributor/positionDisplay.ts
@@ -1,5 +1,6 @@
 import CameraOrientationStateDistributor from "./CameraOrientationStateDistributor.ts";
 import {isInDebug} from "../../../pages/show-scene/debugMode.ts";
+import {vec3GsplatToThree} from "../../vectorUtils.ts";
 
 export const initPositionDisplay = () => {
     if (!isInDebug) {
@@ -8,16 +9,25 @@ export const initPositionDisplay = () => {
 
     document.getElementById("debug-information")!.className = "";
 
-    const debugPosX = document.getElementById("debug-pos-x")!;
-    const debugPosY = document.getElementById("debug-pos-y")!;
-    const debugPosZ = document.getElementById("debug-pos-z")!;
+    const debugPosGsplatX = document.getElementById("debug-pos-gsplat-x")!;
+    const debugPosGsplatY = document.getElementById("debug-pos-gsplat-y")!;
+    const debugPosGsplatZ = document.getElementById("debug-pos-gsplat-z")!;
+
+    const debugPosThreeX = document.getElementById("debug-pos-three-x")!;
+    const debugPosThreeY = document.getElementById("debug-pos-three-y")!;
+    const debugPosThreeZ = document.getElementById("debug-pos-three-z")!
 
     let positionUpdateCount = 0;
     CameraOrientationStateDistributor.addEventListener((state) => {
-        if (positionUpdateCount % 100 == 0) {
-            debugPosX.textContent = state.position.x.toFixed(3);
-            debugPosY.textContent = state.position.y.toFixed(3);
-            debugPosZ.textContent = state.position.z.toFixed(3);
+        if (positionUpdateCount % 10 == 0) {
+            debugPosGsplatX.textContent = state.position.x.toFixed(3);
+            debugPosGsplatY.textContent = state.position.y.toFixed(3);
+            debugPosGsplatZ.textContent = state.position.z.toFixed(3);
+
+            const [threeX, threeY, threeZ] = vec3GsplatToThree(state.position).toArray();
+            debugPosThreeX.textContent = threeX.toFixed(3);
+            debugPosThreeY.textContent = threeY.toFixed(3);
+            debugPosThreeZ.textContent = threeZ.toFixed(3);
 
             console.table(state);
         }
